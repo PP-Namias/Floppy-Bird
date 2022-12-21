792a1085f025648d3c97a6c6fee2d8bdad4dc956
Jhon Keneth Namias <75213409+PP-Namias@users.noreply.github.com>
2022-12-22 02:24:57 +0800

    v1.0.1
    - I've fucked the scoring logic
    - still mad :anger:

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class BirdScript : MonoBehaviour
 {
+    public Rigidbody2D myRigidbody;
+
+    public float flapStrength;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +19,9 @@ public class BirdScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            myRigidbody.velocity = Vector2.up * flapStrength;
+        }
     }
 }
