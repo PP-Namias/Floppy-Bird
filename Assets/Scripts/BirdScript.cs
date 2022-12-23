c2301b208a8434fe6dec4cd3214ba03b889b5513
Jhon Keneth Namias <75213409+PP-Namias@users.noreply.github.com>
2022-12-23 23:16:50 +0800

    v1.0.2
    Game over screen bug

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -5,23 +5,32 @@ using UnityEngine;
 public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
-
     public float flapStrength;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidbody.velocity = Vector2.up * flapStrength;
+
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
         {
             myRigidbody.velocity = Vector2.up * flapStrength;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
     }
 }
